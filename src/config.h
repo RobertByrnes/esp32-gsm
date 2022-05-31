bfbeb408eaf5500e74f6e25076f3b27b59704067
Robert Byrnes <59289145+RobertByrnes@users.noreply.github.com>
2022-05-31 09:10:02 +0100

    Add files via upload

---
+++ src/config.h
@@ -0,0 +1,51 @@
+#include <string>
+
+
+using namespace std;
+
+
+// OAuth Token
+const string OAUTH_HOST = "draperbiotech.clystnet.com";
+const string OAUTH_TOKEN_PATH = "/oauth/token";
+const string GRANT_TYPE = "client_credentials";
+const uint CLIENT_ID = 6;
+const string CLIENT_SECRET = "RN6aJMOXOOSKcNBND2YOKuUQMKlKATgNu3iSYoHn";
+
+// SIM card PIN, leave empty if not defined
+const char *SIM_PIN = ""; 
+
+// End point details for the server
+const char SERVER[] = "draperbiotech.clystnet.com"; // Base url
+const char AUTH_PATH[] = "/oauth/token"; // oAuth token path
+const char RESOURCE_PATH[] = "/api/readings"; // Resource path
+const uint16_t PORT = 443; // Server port number
+
+// location of firmware file on external web server
+// change to your actual .bin location
+const char *UPDATE_HOST = "draperbiotech.clystnet.com";
+const char *UPDATE_URL = "draperbiotech.clystnet.com/public/firmware.bin";
+
+// End point details for the API
+const char DEVICE_ID[] = "";
+
+// GPRS credentials
+const char *APN = "giffgaff.com"; // APN Access Point Name
+const char *GPRS_USER = "gg"; // GPRS User
+const char *GPRS_PASSWORD = "p"; // GPRS Password
+
+// WiFi credentials
+const char *SSID = "CrowdedHouse"; // WiFi Network Name
+const char *PASSWORD = "kF4QMhzc3xcS"; // WiFi Network Password
+
+// Set the server polling time
+// #define CONNECTION_INTERVAL 900000 // every 15mins
+// #define CONNECTION_INTERVAL 300000 // every 5mins
+#define CONNECTION_INTERVAL 60000U // every 1min
+
+// ESP restart counter
+unsigned int RESTART_COUNTER = 0;
+
+// Global variables
+int totalLength;       //total size of firmware
+int currentLength = 0; //current size of written firmware
+unsigned long PREVIOUS_MILLIS = 0;
---
+++ src/esp32-base.cpp
@@ -0,0 +1,154 @@
+#include <Arduino.h>
+#include "config.h"
+#include <CellularNetwork.h>
+#include <OAuth2.h>
+#include <Update.h>
+#include <StreamDebugger.h>
+#include <CRC32.h>
+#include "WiFi_FirmwareUpdater.h"
+
+
+// Modem
+TinyGsm modem(SerialAT);
+CellularNetwork network(APN, GPRS_USER, GPRS_PASSWORD, modem); // explore passing modem by reference
+
+// HTTP(S) Client
+TinyGsmClientSecure client(modem);
+// HttpClient http(client, HOST, 443);
+
+// Authentication
+OAuth2 authHandler(OAUTH_HOST, OAUTH_TOKEN_PATH);
+
+//  Updates
+WiFi_FirmwareUpdater update;
+CRC32 crc;
+
+/**
+ * @brief Connect to the mobile network.
+ * 
+ * @return void
+ */ 
+void makeGSMConnection()
+{
+  if (network.connectNetwork()) {
+      Serial.println("[+] Connected to mobile network OK");
+  } else {
+      Serial.println("[-] failed to connect to mobile network");
+  }
+}
+
+/**
+ * @brief Set modem enable, reset and power pins.
+ * 
+ * @return void
+ */ 
+void setupSIM800L_GPIO()
+{
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
+}
+
+/**
+ * @brief Start WiFi connection.
+ * 
+ * @return void
+ */ 
+// void setupWifiConnection()
+// {
+//   WiFi.mode(WIFI_MODE_STA);
+//   WiFi.begin(SSID, PASSWORD);
+//   while (WiFi.status() != WL_CONNECTED)
+//   {
+//     delay(500);
+//     Serial.print(".");
+//   }
+// }
+
+void setup()
+{
+  Serial.begin(BAUD_RATE);
+
+  setupSIM800L_GPIO();
+
+  Serial.println("[+] Initializing modem...");
+
+  network.initSim(SIM_PIN);
+
+  authHandler.setGrantType(GRANT_TYPE, CLIENT_ID, CLIENT_SECRET);
+}
+
+/**
+ * @brief Connects via mobile network to Draper Biovillam API to collect auth token before
+ * reading DHT sensor and sending all latest readings.  Resets counters.
+ *
+ */
+void connectServer()
+{
+  std::string completeResponse = "";
+  const char *accessToken = "";
+
+  makeGSMConnection();
+  // if (network.isNetworkConnected()) {
+  //   Serial.print("[+] Connected to APN: ");
+  // }
+  
+  Serial.println(APN);
+  Serial.print("[+] Connecting to ");
+  Serial.println(SERVER);
+
+  if (!client.connect(SERVER, PORT)) {
+    Serial.println("[-] Connecting to server failed");
+  } else {
+    Serial.println("[+] Performing HTTP POST request to OAuth Server");
+
+    client.print(authHandler.personalAccessClientTokenRequestString());
+
+    Serial.print("[D] Request string to get token: ");
+    Serial.println(authHandler.personalAccessClientTokenRequestString());
+
+    unsigned long timeout = millis();
+
+    while (client.connected() && millis() - timeout < 10000L) {
+      while (client.available()) {
+        char response = client.read();
+        Serial.print(response);
+        completeResponse += response;
+      }
+    }
+
+    client.stop();
+    Serial.println(F("[+] Server disconnected"));
+
+    network.connection.gprsDisconnect();
+    Serial.println(F("[+] GPRS disconnected"));
+
+    accessToken = authHandler.getToken(completeResponse);
+    Serial.print("[+] Access token: ");
+    Serial.println(accessToken);
+  }
+}
+
+void loop()
+{
+  if (millis() - PREVIOUS_MILLIS >= CONNECTION_INTERVAL) {
+    PREVIOUS_MILLIS = millis();
+    RESTART_COUNTER++;
+    // connectServer();
+
+    makeGSMConnection();
+
+    if (client.connect(SERVER, PORT)) {
+      Serial.print("[+] Connected to host: ");
+      Serial.println(SERVER);
+      update.performUpdate(UPDATE_URL, UPDATE_HOST, PORT, crc, client, network);
+    }
+
+    if (RESTART_COUNTER >= 5) {
+      ESP.restart();
+    }
+  }
+}
