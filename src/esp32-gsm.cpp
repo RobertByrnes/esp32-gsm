dc7d468b771b7c19b347c121edc005402b47998d
RobertByrnes <robbyrnes@hotmail.co.uk>
2022-06-05 22:56:29 +0100

    add memory profiling

--- src/esp32-gsm.cpp
+++ src/esp32-gsm.cpp
@@ -5,8 +5,11 @@
 #include <ESP32TimerInterrupt.h>
 #include <SimpleTimer.h>   
 
-// Timer demonstration
+// User queues
 #define LED_PIN 13
+
+// Memory profiling - uncomment to enable
+#define PROFILE_MEMORY
 
 // ESP hardware timers
 hw_timer_t *timer_1 = NULL; // to run on core 0 - mission critical timed events to run on core 0
@@ -36,18 +39,20 @@ DataUploadApi api(network, client, OAUTH_HOST, OAUTH_TOKEN_PATH);
 // Firmware Updates
 WiFi_FirmwareUpdater update(SSID, PASSWORD, CURRENT_VERSION);
 
+#ifdef PROFILE_MEMORY
 void memoryProfile(std::string taskHandle, TaskHandle_t &task) {
-  Serial.println("*******************************");
+  Serial.println("\n*******************************");
   Serial.printf("App Size: %u\n", ESP.getSketchSize());
   Serial.printf("Free App Space: %u\n", ESP.getFreeSketchSpace());
   Serial.printf("Psram Size: %u\n", ESP.getPsramSize());
   Serial.printf("Free Psram: %u\n", ESP.getFreePsram());
   Serial.printf("Heap Size: %u\n", ESP.getHeapSize()); 
   Serial.printf("Free Heap: %u\n", ESP.getFreeHeap());
-  Serial.print(String(taskHandle.c_str()) + "task_1, stack not used: ");
+  Serial.print(String(taskHandle.c_str()) + " stack not used: ");
   Serial.println(uxTaskGetStackHighWaterMark(task));
   Serial.println("*******************************");
 }
+#endif
 
 /**
  * @brief Interrupt service routine callback func,
@@ -111,7 +116,9 @@ static void core_0_task(void *pvParameters)
 
   for (;;) {
     if (interruptCounter_1 > 0) {
+#ifdef PROFILE_MEMORY
       memoryProfile("task_1", task_1);
+#endif
       portENTER_CRITICAL(&timerMux_1);
       interruptCounter_1--;
       portEXIT_CRITICAL(&timerMux_1);
@@ -152,7 +159,9 @@ static void core_1_task(void *pvParameters)
 
   for (;;) {
     if (interruptCounter_2 > 0) {
+#ifdef PROFILE_MEMORY
       memoryProfile("task_2", task_2);
+#endif
       portENTER_CRITICAL(&timerMux_2);
       interruptCounter_2--;
       portEXIT_CRITICAL(&timerMux_2);
@@ -204,26 +213,6 @@ void setup()
       &task_2, // Task handle to keep track of created task
       1 // pin task to core 1
   ); 
-
-  // Serial.print("[D] task_1, stack not used: ");
-  // Serial.println(uxTaskGetStackHighWaterMark(task_1));
-
-  // Serial.print("[D] task_2, stack not used: ");
-  // Serial.println(uxTaskGetStackHighWaterMark(task_2));
 }
 
-void loop()
-{
-  vTaskDelete(NULL);
-
-  // Memory Use Debug
-  // unsigned long currentMillis = millis();
-
-  // if (currentMillis - previousMillis > printInterval) {
-  //   previousMillis = currentMillis; 
-  //   Serial.printf("[D] Free app space: %u\n", ESP.getFreeSketchSpace());
-  //   Serial.printf("[D] Free Psram: %u\n", ESP.getFreePsram()); 
-  //   Serial.printf("[D] Free Heap: %u\n", ESP.getFreeHeap());
-  // }
-  
-}
+void loop() { vTaskDelete(NULL); }
